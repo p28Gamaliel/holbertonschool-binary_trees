53de9395c2fd49f978802933c6aaed971fee7a6a
p28Gamaliel <gamaliel13perez@gmail.com>
2025-08-08 13:45:02 +0000

    leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - count the leaves on the tree
+ * @tree: pointer to the next node
+ * Return: If tree is NULL, the function must return 0
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
